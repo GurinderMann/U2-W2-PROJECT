2a35654a7a9d3343d1979b2ecaa94dc66cdcdca6
GurinderMann <manngurinder110@gmail.com>
2023-06-30 16:26:42 +0200

    finished

---
+++ script.js
@@ -0,0 +1,12 @@
+const edit = document.getElementById('edit')
+edit.addEventListener('click',function (e){
+    window.location.href = '/EditProfile.html'
+})
+
+const settings = document.getElementById('Settings')
+
+settings.addEventListener('click',function (e){
+    window.location.href = '/settings.html'
+})
+
+
